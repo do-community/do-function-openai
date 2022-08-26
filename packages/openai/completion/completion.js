5f94aacb902330cbb0b9268c0ae4e7c8d27b3d6f
Ado Kukic <kukicadnan@gmail.com>
2022-08-26 09:29:56 -0700

    OpenAI Example

---
+++ packages/openai/completion/completion.js
@@ -0,0 +1,31 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+    apiKey: process.env['apiKey'],
+});
+
+const openai = new OpenAIApi(configuration);
+
+
+async function main(args) {
+    console.log(args);
+    if (args.__ow_method !== 'post'){
+        return { "body": "Please send a POST Request"};
+    }
+    if (args.prompt){
+        const completion = await openai.createCompletion({
+            model: "text-davinci-002",
+            prompt: args.prompt,
+            max_tokens: args.max_tokens || 16,
+            temperature: args.temperature || 0.9,
+            top_p: args.top_p || 1
+          });
+          console.log(completion.data.choices[0].text);
+        
+        return {"body": completion.data.choices[0].text}
+    } else {
+        return {"body": "Please send a prompt"}
+    }
+  }
+
+exports.main = main
